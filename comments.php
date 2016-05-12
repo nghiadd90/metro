8dab9dc677bfc9b2ec7b16f8b82f3c519e5ab626
Dau Dinh Nghia <dau.dinh.nghia@framgia.com>
2016-05-12 17:12:43 +0700

    add commment file

---
+++ comments.php
@@ -0,0 +1,45 @@
+<div class="frg-comment-box">
+<?php if (have_comments()) : ?>
+    <div class="well">
+    <h3 id="comments">
+        <?php
+        if (1 == get_comments_number()) {
+            /* translators: %s: post title */
+            printf(__('Một bình luận', 'metro'));
+        } else {
+            /* translators: 1: number of comments, 2: post title */
+            printf(
+                _n('Có %1$s bình luận', 'Có %1$s bình luận', get_comments_number()),
+                number_format_i18n(get_comments_number())
+            );
+        }
+        ?>
+    </h3>
+    </div>
+    <div class="navigation">
+        <div class="alignleft"><?php previous_comments_link() ?></div>
+        <div class="alignright"><?php next_comments_link() ?></div>
+    </div>
+    <div class="frg-comment-list">
+        <ul class="commentlist">
+        <?php wp_list_comments();?>
+        </ul>
+    </div>
+    <div class="navigation">
+        <div class="alignleft"><?php previous_comments_link() ?></div>
+        <div class="alignright"><?php next_comments_link() ?></div>
+    </div>
+    <?php else : // this is displayed if there are no comments so far ?>
+
+    <?php if (comments_open()) : ?>
+        <!-- If comments are open, but there are no comments. -->
+
+        <?php else : // comments are closed ?>
+        <!-- If comments are closed. -->
+        <p class="nocomments"><?php _e('Bình luận đã đóng.'); ?></p>
+
+    <?php endif; ?>
+<?php endif; ?>
+
+<?php comment_form(); ?>
+</div>
