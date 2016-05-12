7f47748f7d425073f8ef801230c42f18a2c7a1c6
Dau Dinh Nghia <dau.dinh.nghia@framgia.com>
2016-05-12 17:16:08 +0700

    add search template file

---
+++ content/content-search.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * The template part for displaying results in search pages
+ *
+ * @package Metro
+ * @subpackage Metro
+ * @since Metro 1.0
+ */
+?>
+                                    <div class="col-xs-6 col-sm-6">
+                                        <div class="thumbnail">
+                                            <h4>
+                                            <?php
+                                            $category = get_the_category();
+                                            echo $category[0]->cat_name;
+                                            ?>
+                                            </h4>
+                                            <?php
+                                            if (has_post_thumbnail()) {
+                                                echo '<a href="' . get_permalink() . '">';
+                                                the_post_thumbnail('category-image');
+                                                echo '</a>';
+                                            }
+                                            ?>
+                                            <div class="caption">
+                                                <h4><a href="<?php echo get_permalink(); ?>"><?php the_title(); ?></a></h4>
+                                                <p><?php echo string_limit_words(get_the_excerpt(), 20) . '...'; ?></p>
+                                            </div>
+                                        </div>
+                                    </div>
+
